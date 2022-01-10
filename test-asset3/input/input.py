fd0862e0196257260137559931693227e6ddac70
ssoon.lee <ssoon.lee@lge.com>
2022-01-10 04:22:47 +0000

    update

--- test-asset3/input/input.py
+++ test-asset3/input/input.py
@@ -1,5 +1,12 @@
 import sys
+import argparse
 
+parser = argparse.ArgumentParser(argparse.SUPPRESS)
+parser.add_argument("--a", required=True)
+parser.add_argument("--e", required=True)
+parser.add_argument("--c", required=True)
+parser.add_argument("--integer_parameter", required=True)
+FLAGS = parser.parse_args()
 print ("###################### input start ")
-print (sys.argv)
+print ('a : {} / e : {} / c : {} / integer_parameter : {}'.format(FLAGS.a, FLAGS.e, FLAGS.c, FLAGS.integer_parameter))
 print ("###################### input end")
