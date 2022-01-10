f8159f99635d989447883d1072f52b8d87e527be
ssoon.lee <ssoon.lee@lge.com>
2022-01-10 04:31:23 +0000

    arg test

--- test-asset3/input/input.py
+++ test-asset3/input/input.py
@@ -4,7 +4,7 @@ import argparse
 parser = argparse.ArgumentParser(argparse.SUPPRESS)
 parser.add_argument("--a", required=True)
 parser.add_argument("--e", required=True)
-parser.add_argument("--c", required=True)
+parser.add_argument("--c", type=str, required=True)
 parser.add_argument("--integer_parameter", required=True)
 FLAGS = parser.parse_args()
 print ("###################### input start ")
